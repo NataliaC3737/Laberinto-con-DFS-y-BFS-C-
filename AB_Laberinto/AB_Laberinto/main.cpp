3b4a6fdc11354bf56e1931e2491bb48b507d5bf1
caleronatalia3737 <caleronatalia3737@gmail.com>
2024-01-08 21:25:26 +0100

    Mensajes y organización en terminal

--- AB_Laberinto/AB_Laberinto/main.cpp
+++ AB_Laberinto/AB_Laberinto/main.cpp
@@ -7,31 +7,32 @@
 #include "laberinto.h" // Incluimos el archivo de funciones del laberinto
 
 int main() {
-    int opcion;
-    std::cout << "Elige el modo de resolucion del laberinto:" << std::endl;
-    std::cout << "1. BFS" << std::endl;
-    std::cout << "2. DFS" << std::endl;
-    std::cin >> opcion;
-
     std::vector<std::vector<int>> laberinto; // Matriz de adyacencia para representar el laberinto
     int n = 10; // Tamaño de la matriz (ajústalo según el tamaño de tu laberinto)
     definirLaberinto(n, laberinto); // Definir el laberinto según la estructura
     std::vector<std::vector<std::string>> interfaz;
 
-    if (opcion == 1) {
-        std::cout << "Resolviendo el laberinto usando BFS..." << std::endl;
-        resolverLaberintoBFS(laberinto); // Resolver el laberinto usando BFS
-        mostrarLaberinto(interfaz);
-    }
-    else if (opcion == 2) {
-        std::cout << "Resolviendo el laberinto usando DFS..." << std::endl;
-        resolverLaberintoDFS(laberinto); // Resolver el laberinto usando DFS
-        mostrarLaberinto(interfaz);
-    }
-    else {
-        std::cout << "Opción no válida. Por favor, elige 1 o 2." << std::endl;
-    }
+    std::cout << "Bienvenido/a al laberinto" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "------------------------Instrucciones------------------------" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "A continuacion se muestra la resolucion por medio de los " << std::endl;
+    std::cout << "algoritmos de busqueda en profundidad(DFS) y amplitud(BFS)." << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "El nodo (0) representa el inicio, y el nodo (5) el final." << std::endl;
+    std::cout << " " << std::endl;
 
+    std::cout << "-------------------------Soluciones--------------------------" << std::endl;
+    std::cout << " " << std::endl;
+    resolverLaberintoBFS(laberinto); // Resolver el laberinto usando BFS
+    std::cout << " " << std::endl;
+    resolverLaberintoDFS(laberinto); // Resolver el laberinto usando DFS
+    std::cout << " " << std::endl;
+
+    std::cout << "--------------------------Laberinto--------------------------" << std::endl;
+    std::cout << " " << std::endl;
+    mostrarLaberinto(interfaz);
+    
     return 0;
 }
 
